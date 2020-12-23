c6c41a29398f218b80a7077b1469fd4620d968b8
Nominprogramer <58177990+Nominprogramer@users.noreply.github.com>
2020-12-23 12:11:05 +0530

    Update window.py

--- window.py
+++ window.py
@@ -1,13 +1,20 @@
-def createWindow(height,width,colour):
+#define function using def
+def createWindow(height,width,colour):#arguments and ":" for writing the code
+    #importing tkinter for graphics
     import tkinter as tk
+    #root is base of ui
     root = tk.Tk()
+    #TITLING OUR WINDOW
     root.title("PieEngine")
+    #OUR CANVAS
     c = tk.Canvas(root,bg = colour,height = height,width = width)
+    #DISPLAYING OUR CANVAS
     c.pack()
+    #MAINLOOP
     root.mainloop() 
 
 
 
 
-
+#CALLING THE FUNCTION
 createWindow(400,400,None)
